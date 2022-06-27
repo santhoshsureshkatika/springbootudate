bf23b08bfd80cd1910d2ddf2d161b87449fcc986
santhoshsureshkatika <105355923+santhoshsureshkatika@users.noreply.github.com>
2022-06-27 09:25:28 +0530

    Exception Handler Done

---
+++ Coupon  -with Exception Handler/src/main/java/com/zensar/dto/CouponDto.java
@@ -0,0 +1,69 @@
+package com.zensar.dto;
+
+public class CouponDto {
+	private int couponId;
+	private String couponCode;
+	private String expDate;
+	private int couponPrice;
+	private String couponDesc;
+
+	public CouponDto() {
+		super();
+	}
+
+	public CouponDto(int couponId, String couponCode, String expDate, int couponPrice, String couponDesc) {
+		super();
+		this.couponId = couponId;
+		this.couponCode = couponCode;
+		this.expDate = expDate;
+		this.couponPrice = couponPrice;
+		this.couponDesc = couponDesc;
+	}
+
+	public int getCouponId() {
+		return couponId;
+	}
+
+	public void setCouponId(int couponId) {
+		this.couponId = couponId;
+	}
+
+	public String getCouponCode() {
+		return couponCode;
+	}
+
+	public void setCouponCode(String couponCode) {
+		this.couponCode = couponCode;
+	}
+
+	public String getExpDate() {
+		return expDate;
+	}
+
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
+	}
+
+	public int getCouponPrice() {
+		return couponPrice;
+	}
+
+	public void setCouponPrice(int couponPrice) {
+		this.couponPrice = couponPrice;
+	}
+
+	public String getCouponDesc() {
+		return couponDesc;
+	}
+
+	public void setCouponDesc(String couponDesc) {
+		this.couponDesc = couponDesc;
+	}
+
+	@Override
+	public String toString() {
+		return "CouponDto [couponId=" + couponId + ", couponCode=" + couponCode + ", expDate=" + expDate
+				+ ", couponPrice=" + couponPrice + ", couponDesc=" + couponDesc + "]";
+	}
+
+}
---
+++ Coupon  -with Exception Handler/src/main/java/com/zensar/exceptions/NoSuchCouponExistsException.java
@@ -0,0 +1,18 @@
+package com.zensar.exceptions;
+
+public class NoSuchCouponExistsException extends RuntimeException{
+	private String message;
+
+	public NoSuchCouponExistsException() {
+		super();
+		
+	}
+
+	public NoSuchCouponExistsException(String message) {
+		super(message);
+		this.message = message;
+	}
+	
+
+
+}
